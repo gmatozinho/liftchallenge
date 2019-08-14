c41fb961b6ac65cc5b6290ce4cd8074bc4ca7837
Gustavo Matozinho <gustavomatozinho@gmail.com>
2019-08-13 21:54:40 -0300

    refactor and init mock

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const usersRoutes = require('./controllers/users');
 
 var app = express();
 
@@ -13,8 +13,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/users', usersRoutes.index);
 
 module.exports = app;
---
+++ controllers/users.js
@@ -0,0 +1,15 @@
+exports.index = (req, res) => {
+  let favJson = {
+    "foo": "Pizza",
+    "bar": "Roast Pork Kuckle"
+  };
+  res.send(favJson);
+};
+
+exports.login = (req, res) => {
+  let favJson = {
+    "foo": "Pizza",
+    "bar": "Roast Pork Kuckle"
+  };
+  res.send(favJson);
+};
